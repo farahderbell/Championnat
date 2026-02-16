3ff470d20f84654efe6321c28c5054fe8cc861e6
farahderbell <150382054+farahderbell@users.noreply.github.com>
2026-02-16 12:19:28 +0100

    added imports

--- src/main/java/tn/esprit/ds/championnat/controllers/EquipeController.java
+++ src/main/java/tn/esprit/ds/championnat/controllers/EquipeController.java
@@ -3,8 +3,10 @@ package tn.esprit.ds.championnat.controllers;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tn.esprit.ds.championnat.entities.Equipe;
 import tn.esprit.ds.championnat.services.IEquipeService;
 
 @AllArgsConstructor
