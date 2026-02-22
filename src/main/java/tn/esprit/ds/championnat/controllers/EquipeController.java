133772ef1c6f2b3248d376afce0066f4b5e565a6
farahderbell <150382054+farahderbell@users.noreply.github.com>
2026-02-22 19:16:22 +0100

    exposed the whole equipe crud in the equipecontroller

--- src/main/java/tn/esprit/ds/championnat/controllers/EquipeController.java
+++ src/main/java/tn/esprit/ds/championnat/controllers/EquipeController.java
@@ -2,12 +2,12 @@ package tn.esprit.ds.championnat.controllers;
 
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tn.esprit.ds.championnat.entities.Equipe;
+import tn.esprit.ds.championnat.entities.Pilote;
 import tn.esprit.ds.championnat.services.IEquipeService;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -22,4 +22,34 @@ public class EquipeController {
     public Equipe ajouterEquipe(@RequestBody Equipe equipe) {
         return equipeService.ajouterEquipe(equipe);
     }
+
+
+    @PostMapping("/add-multiple-equipe")
+    public List<Equipe> ajouterEquipes(@RequestBody List<Equipe> equipes) {
+        return equipeService.ajouterequipes(equipes);
+
+    }
+
+    @PutMapping("/update-equipe")
+    public Equipe modifierSponsor(@RequestBody Equipe s) {
+        return equipeService.modifierEquipe(s);
+    }
+
+
+    @GetMapping("/get-all-equipe")
+    public List<Equipe> listequipes() {
+        return equipeService.listEquipes();}
+
+    @DeleteMapping("/delete-equipe/{id}")
+    public void supprimerEquipe(@PathVariable Long id) {
+        equipeService.supprimerEquipe(id);
+    }
+
+
+    @GetMapping("/get-equipe/{id}")
+    public Equipe recupererEquipe(@PathVariable("id") Long idEquipe) {
+        return equipeService.recupererEquipe(idEquipe);
+    }
+
+    
 }
